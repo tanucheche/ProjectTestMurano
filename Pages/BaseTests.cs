970c86b67266cb06808eb83b2111e159de1fb05b
tanucheche <tanuchemail@gmail.com>
2021-06-16 15:24:13 +0300

    2 commit

---
+++ Pages/BaseTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestProjectMuramo
+{
+    [TestFixture]
+    public class BaseTests
+    {
+        protected static IWebDriver driver => DriverManager.driver;
+        
+        [SetUp]
+        public void SetUpBrowser()
+        {
+            DriverManager.StartBrowser(); 
+        }
+        [Test]
+        public void Test1()
+        {
+            IWebElement course = driver.FindElement(By.Id("Language"));
+            var selectTest = new SelectElement(course);
+            // Select a value from the dropdown				
+            selectTest.SelectByValue("VbNet");
+        }
+
+        [TearDown]
+        public void TearDownBrowser()
+        {
+            DriverManager.CloseBrowser();
+        }
+    }
+}
---
+++ Pages/EditorPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace TestProjectMuramo
+{
+    public class EditorPage : BaseTests
+    {
+        //здесь будут тесты на редактирование страницы
+       [Test]
+      public void Test1()
+        {
+             IWebElement course = driver.FindElement(By.Id("Language"));
+             var selectTest = new SelectElement(course);
+            //Select a value from the dropdown				
+             selectTest.SelectByValue("VbNet");
+            
+        }
+    }
+}
